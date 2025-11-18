b853b389f4ebb8f19a991eac7ae49b09d26ff428
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2025-11-19 00:20:07 +0800

    fix: fixed a bug where it catches an exception

--- src/gui/productreturnanalysis.php
+++ src/gui/productreturnanalysis.php
@@ -89,7 +89,7 @@
             $query = "SELECT o.order_id, p.product_name, SUM(o.quantity) AS total_refunded
                       FROM products p
                       JOIN orders o ON p.product_id = o.product_id
-                      GROUP BY p.product_id, p.product_name
+                      GROUP BY o.order_id, p.product_name
                       ORDER BY total_refunded DESC, p.product_id ASC";  
             $result = mysqli_query($conn, $query);
 
